a822d75649975d71def215eae80222b81f1762db
Bongo117 <luchosoto117@gmail.com>
2025-08-22 20:34:14 -0300

    probando las vistas

--- Inmobiliaria.MVC/obj/Debug/net8.0/Inmobiliaria.MVC.AssemblyInfo.cs
+++ Inmobiliaria.MVC/obj/Debug/net8.0/Inmobiliaria.MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inmobiliaria.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc9184cdbfcd7c36582e27a38a7fa7528fa18680")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+402ec12b7d65915f8de822a1554869708942c4ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inmobiliaria.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inmobiliaria.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
